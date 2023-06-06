73ece93173d07e135336f19680630601fe4b67f7
test <test@ubuntu-vm-ixnet-automation.ccu.is.keysight.com>
2023-06-06 16:14:20 +0530

    chnages to test run loggin

--- test_setup/test_runner.py
+++ test_setup/test_runner.py
@@ -1,6 +1,6 @@
 from os import system
 class Tests() :
-    def __init__(self, tests, path, mode="systest",Timeout = 120, suite="BGP") :
+    def __init__(self, tests, path, logging, mode="systest",Timeout = 120, suite="BGP") :
         self.timeout = "120"
         self.no_of_tests = 0
         self.suite = suite
@@ -9,7 +9,9 @@ class Tests() :
         self.tests = tests
         self.test_to_run = ""
         self.path = path
+        self.logging = logging
     
+
     def get_test(self):
         if self.suite =="BGP":
             self.no_of_tests = len(self.tests["BGP"])
@@ -17,14 +19,22 @@ class Tests() :
     
     def run_test(self):    
         for test in self.test_to_run:
+            self.show_details(test)
             no_of_devices = self.test_to_run[test]["no_of_devices"]
             no_of_ports = self.test_to_run[test]["no_of_ports"]
             no_of_flows = self.test_to_run[test]["noofflows"]
             no_of_pkts = self.test_to_run[test]["noofpkts"]
             frame_size = self.test_to_run[test]["framesize"]
             cmd1 = "cd %s\n" % (self.path)
-            cmd2 = "python3 -m pytest -k test_bgp.py"
+            cmd2 = "python3 -m pytest -k test_bgp.py --noofdevices %s --noofports %s --noofpkts %s --noofflows %s --framesize %s --sessiontimeout %s" \
+                % (no_of_devices, no_of_ports, no_of_pkts, no_of_flows, frame_size, self.timeout)
             cmd = cmd1 + cmd2
             system(cmd)
+    
+    def show_details(index):
+        self.logging.info("Running test for %s" % (self.suite))
+        self.logging.info("Running test with these arguments")
+        self.logging.info(self.test_to_run[index])
+
 
             
